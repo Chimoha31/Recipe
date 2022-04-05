4e21af6b93090bee6e7a6a2ff55e252c23e52f18
Chimoha31 <mokochii1108@gmail.com>
2022-04-05 00:39:42 -0700

    List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./List.css";
+import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
+import Typography from "@mui/material/Typography";
 
 function List() {
+  const APP_ID = 
+  const APP_KEY = 
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  };
+
+  useEffect(() => {
+    console.log(inputValue);
+  }, [inputValue])
+
   return (
-    <div>
-      <h1>List</h1>
+    <div className="list_container">
+      <div className="search">
+        <Input
+          placeholder="Placeholder"
+          onChange={handleChange}
+          value={inputValue}
+        />
+        <Button variant="contained">
+          <Typography variant="p">Search</Typography>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
