ca95c8494aa8d4ae7a367de7ff1dda19e3633b1e
Chimoha31 <mokochii1108@gmail.com>
2022-04-05 00:13:27 -0700

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              Recipe
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   )
 }
